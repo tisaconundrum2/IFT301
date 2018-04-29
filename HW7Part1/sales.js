a30ab15bb905e797ef6aded3a40db3267d48dbd2
Tisaconundrum2 <ngf4@nau.edu>
2018-04-29 16:28:47 -0700

    Adding initial files for HW7

---
+++ HW7Part1/sales.js
@@ -0,0 +1,15 @@
+var r1 = [1540, 1130, 1580, 1105];
+var r2 = [2010, 1168, 2305, 4102];
+var r3 = [2450, 1847, 2710, 2391];
+var r4 = [1845, 1491, 1284, 1575];
+var r5 = [2120, 1767, 1599, 3888];
+var textDisplay;
+
+var $ = function (id) {
+	return document.getElementById(id);
+}
+window.onload = function () {
+
+}
+
+
